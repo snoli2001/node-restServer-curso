bb7fe3d568bd00238a62a121aae19a8224c3d95a
Sebastian <sebastian.noli@gmail.com>
2021-01-31 11:56:19 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -6,3 +6,23 @@
 //          Puerto
 // ==========================
 process.env.PORT = process.env.PORT || 3000;
+
+// ==========================
+//          Entorno
+// ==========================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ==========================
+//          Base de datos
+// ==========================
+
+let urlDB;
+
+if( process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://snoli2001:s-noli457@cluster0.7idal.mongodb.net/cafe?authSource=admin&replicaSet=atlas-ulpcn2-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 require('./config/config')
 
+
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
 const bodyParser = require('body-parser')
 
@@ -11,37 +13,16 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function (req, res) {
-    res.json('get Usuario');
-});
 
-app.post('/usuario', function (req, res) {
+mongoose.connect(process.env.URLDB,
+    { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology:true},
+    (err, res) =>{
+    if ( err ) throw err;
 
-    let body = req.body;
-    if ( body.nombre === undefined){
-        res.status(400).json({
-            ok:false,
-            mensaje: `El nombre es necesario`
-        })
-    } else {
-        res.json({
-            persona: body
-        });
-    }
+    console.log('Base de datos ONLINE');
+} );
 
-});
-
-app.put('/usuario/:id', function (req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete Usuario');
-});
-
+app.use( require('./routes/usuario') );
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto:', process.env.PORT)
