1db4fb712d2fb0c5b6aaf71ff0517a3e0e308b02
Sebastian <sebastian.noli@gmail.com>
2021-01-31 20:18:50 -0500

    comillas dobles prueba

--- server/config/config.js
+++ server/config/config.js
@@ -20,9 +20,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB;
 
 if( process.env.NODE_ENV === 'dev'){
-    urlDB = 'mongodb://localhost:27017/cafe';
+    urlDB = "mongodb://localhost:27017/cafe";
 } else {
-    urlDB = 'mongodb+srv://snoli2001:s-noli457@cluster0.7idal.mongodb.net/cafe?authSource=admin&replicaSet=atlas-ulpcn2-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+    urlDB = "mongodb+srv://snoli2001:s-noli457@cluster0.7idal.mongodb.net/cafe?authSource=admin&replicaSet=atlas-ulpcn2-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
 }
 
 process.env.URLDB = urlDB;
