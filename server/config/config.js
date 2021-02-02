9a7237f19d5c35f246ad555b3a34ab9a0e3ec243
Sebastian <sebastian.noli@gmail.com>
2021-02-02 10:19:12 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -22,7 +22,7 @@ let urlDB;
 if( process.env.NODE_ENV === 'dev'){
     urlDB = "mongodb://localhost:27017/cafe";
 } else {
-    urlDB = "mongodb+srv://snoli2001:s-noli457@cluster0.7idal.mongodb.net/cafe";
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
